6465164b7191539250fbae3654573fe958e70ed3
Coding Team <87824092+codingtuto@users.noreply.github.com>
2022-02-25 13:18:33 +0000

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,12 @@
+class Translation(object):
+
+    START_MSG = """👋Saluy {}\n\n Je suis Google Traducteur. \n Je peux traduire n'importe quelle langue dans la langue de votre choix"""
+
+    TRANSLATED_MSG = """Choisissez la langue d'ici que je veux traduire.👇"""
+
+    ABOUT_MSG = """🤖 Bot : Google Traducteur\n\n👨‍🎓 Développeur : @A_liou\n\n🎙️ Langage : Python3\n\n📚 Librairie : Pyrogram v1.2.9\n\n🛑 Serveur : Heroku\n\n📱 Version :V1.0"""
+
+    CREDITS = """Crédits ici"""
+
+
+
